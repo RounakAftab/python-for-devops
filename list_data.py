98f6026d01c71755b136a1b0dd140c6218926f02
rounak <rounak.aftab@gmail.com>
2025-01-06 02:37:54 -0500

    add list data

---
+++ list_data.py
@@ -0,0 +1,20 @@
+my_list= ['rounak', 'roza', 'azvan']
+
+print(my_list[-1])
+
+# append add last in the list
+
+# my_list.append('ruchi')
+# print(my_list)
+
+# insert add data in the specified position
+
+# my_list.insert(1, 'ruchi')
+# print(my_list)
+
+my_list1= ['a','b','c']
+my_list2= ['d','e','f']
+
+combined_list=my_list1 + my_list2
+
+print(combined_list)
