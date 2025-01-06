9de16ee4f3e459779d18592f1fc99de9eff86203
rounak <rounak.aftab@gmail.com>
2025-01-06 01:56:50 -0500

    add conditions

---
+++ conditions.py
@@ -0,0 +1,24 @@
+# if, else, elseif
+
+
+# temp = int(input('type todays temp= '))
+
+# if temp > 25:
+
+#     print('Its too hot today')
+
+# else:
+#     print("Its confortable weather")        
+
+
+temp = int(input('type todays temp= '))
+
+if temp > 40:
+
+    print('Its too hot today')
+
+elif 30 <= temp <= 40:
+    print("its worm wearther, wear light cloth")
+
+else:
+    print("Its confortable weather") 
