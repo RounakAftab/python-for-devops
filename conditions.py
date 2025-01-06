24cba3ee3127721c40444a5d2b1e10297cf9f79d
rounak <rounak.aftab@gmail.com>
2025-01-06 02:21:58 -0500

    add conditions task for demo

--- conditions.py
+++ conditions.py
@@ -10,15 +10,30 @@
 # else:
 #     print("Its confortable weather")        
 
+#  ----------------------------
 
-temp = int(input('type todays temp= '))
+# temp = int(input('type todays temp= '))
 
-if temp > 40:
+# if temp > 40:
 
-    print('Its too hot today')
+#     print('Its too hot today')
 
-elif 30 <= temp <= 40:
-    print("its worm wearther, wear light cloth")
+# elif 30 <= temp <= 40:
+#     print("its worm wearther, wear light cloth")
+
+# else:
+#     print("Its confortable weather") 
+
+input_taker = int(input('Please input a number: '))
+
+if input_taker % 3 == 0 & input_taker % 5== 0:
+    print('FizzBuzz')
+
+elif input_taker % 3 == 0:
+    print('Fizz')
+
+elif input_taker % 5 ==0:
+    print('Buzz')
 
 else:
-    print("Its confortable weather") 
+    print('output not a FizzBuzz number')
